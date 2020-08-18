aad6b69deb497cf0c603659ca19c1a915cc8a6eb
Vlad <v.rozanoff@gmail.com>
2020-08-19 00:01:34 +0300

    return jsx rewrite component as a function

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props =>  {
+  const [ personsState, setPersonsState] = useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value'
-  }
+  });
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working</p>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age} 
+       />
+      <Person 
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age}> 
+      My Hobbies: Racing
+      </Person>
+      <Person
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age} />
+    </div>
+  );
 
   switchNameHandler = () => {
     // console.log('Was clicked !');
@@ -24,19 +44,15 @@ class App extends Component {
     })
   }
 
-  
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-      </div>
-    );
-  }
 }  
-export default App;
+export default app;
+
+
+state = {
+  persons: [
+    {name: 'Max', age: 28},
+    {name: 'Manu', age: 29},
+    {name: 'Stephanie', age: 26}
+  ],
+  otherState: 'some other value'
+}
