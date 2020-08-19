98892c2fa6a4befbaed94caaa4c7ef7b1914cebd
Vlad <v.rozanoff@gmail.com>
2020-08-19 09:59:50 +0300

    Move parts of module

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,47 +12,45 @@ const app = props =>  {
     otherState: 'some other value'
   });
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working</p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person
-       name={this.state.persons[0].name} 
-       age={this.state.persons[0].age} 
-       />
-      <Person 
-      name={this.state.persons[1].name} 
-      age={this.state.persons[1].age}> 
-      My Hobbies: Racing
-      </Person>
-      <Person
-       name={this.state.persons[1].name} 
-       age={this.state.persons[1].age} />
-    </div>
-  );
+  
 
-  switchNameHandler = () => {
+  console.log(personsState);
+
+  const switchNameHandler = () => {
     // console.log('Was clicked !');
     // this.state.persons[0].name = 'Vlad';
-    this.setState( {
+    setPersonsState( {
       persons: [
         {name: 'Vlad', age: 28},
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 26}
-      ]
-    })
-  }
+      ],
+    });
+  };
 
-}  
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+       name={personsState.persons[0].name} 
+       age={personsState.persons[0].age} 
+       />
+      <Person 
+      name={personsState.persons[1].name} 
+      age={personsState.persons[1].age}> 
+      My Hobbies: Racing
+      </Person>
+      <Person
+       name={personsState.persons[1].name} 
+       age={personsState.persons[1].age} />
+    </div>
+  );
+};
 export default app;
 
 
-state = {
-  persons: [
-    {name: 'Max', age: 28},
-    {name: 'Manu', age: 29},
-    {name: 'Stephanie', age: 26}
-  ],
-  otherState: 'some other value'
-}
+
+
+
