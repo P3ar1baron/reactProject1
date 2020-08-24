8319aeb048c17ecf158e842b9255d71bb0c46a8d
Vlad <v.rozanoff@gmail.com>
2020-08-24 22:42:40 +0300

    Fix indexes Add props.changed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -44,14 +44,14 @@ switchNameHandler = (newName) => {
       <Person
        name={this.state.persons[0].name} 
        age={this.state.persons[0].age} />
-       <Person 
+      <Person 
       name={this.state.persons[1].name} 
       age={this.state.persons[1].age}
       click = {this.switchNameHandler.bind(this,'Sanya')}
       changed= {this.nameChangedHandler}>My Hobbies: Racing</Person>
       <Person
-       name={this.state.persons[1].name} 
-       age={this.state.persons[1].age} />
+       name={this.state.persons[2].name} 
+       age={this.state.persons[2].age} />
     </div>
   );
  }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -8,7 +8,7 @@ const person = (props) => {
         <div>
          <p onClick ={props.click}>I'm a {props.name} and I am {props.age} !</p>
          <p>{props.children}</p>
-         <input type="text" onChange=""/>
+         <input type="text" onChange= {props.changed} />
         </div>
     )
 };
